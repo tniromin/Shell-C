4973840798754c991a03bb93a9e134a698bedcf4
tniromin <tniromin2@gmail.com>
2025-01-07 06:44:35 +0000

    FEAT: INIT ERROR EXIT

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+  // Flush after every std::cout / std:cerr
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
+
+  while(true){
+
+    std::cout << "$ ";
+    std::string input;
+    std::getline(std::cin, input);
+    if(input=="exit 0") return 0;
+    std::cout << input << ": command not found" << std::endl;
+    
+  }
+
+}
