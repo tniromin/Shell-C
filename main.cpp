ed84644072f70d221577849d05d63b8571a0254d
tniromin <tniromin2@gmail.com>
2025-01-07 07:05:16 +0000

    FEAT: ECHO

--- main.cpp
+++ main.cpp
@@ -10,8 +10,17 @@ int main() {
     std::cout << "$ ";
     std::string input;
     std::getline(std::cin, input);
-    if(input=="exit 0") return 0;
-    std::cout << input << ": command not found" << std::endl;
+    // if(input=="exit 0") return 0;
+    // std::cout << input << ": command not found" << std::endl;
+   if (input == "exit 0") {
+      return 0;
+    } else if (input.find("echo ") == 0) {
+      const int ECHO_LEN = 5; // Length of "echo "
+      std::string text = input.substr(ECHO_LEN);
+      std::cout << text << std::endl;
+    } else {
+      std::cout << input << ": command not found" << std::endl;
+    }
     
   }
 
